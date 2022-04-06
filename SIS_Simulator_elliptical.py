1b7b08d046b9de5c68f4a6a1c6e6ae9f197690be
eduvaladao <43937108+eduvaladao@users.noreply.github.com>
2022-04-06 00:14:48 -0300

    Add files via upload

--- SIS_Simulator_elliptical.py
+++ SIS_Simulator_elliptical.py
@@ -7,24 +7,24 @@ import matplotlib.colors
 cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["black","rebeccapurple","white"])
 
 
-print "*-----------------------------------------------------------------------------------------*"
-print "| SIS Surface Brightness Simulator for spiral and elliptical galaxies (elliptical source) |"
-print "*-----------------------------------------------------------------------------------------*" 
+print ("*-----------------------------------------------------------------------------------------*")
+print ("| SIS Surface Brightness Simulator for spiral and elliptical galaxies (elliptical source) |")
+print ("*-----------------------------------------------------------------------------------------*")
 
-print "Insira os valores para o centro da fonte de coordenadas cartesianas S1 e S2:"
+print ("Insira os valores para o centro da fonte de coordenadas cartesianas S1 e S2:")
 S1 = float(input("S1 = "))
 S2 = float(input("S2 = ")) 
 
-print "Insira o valor para a excentricidade da sua fonte elíptica entre 0 e 1, sendo 1 não incluído:"
+print ("Insira o valor para a excentricidade da sua fonte elíptica entre 0 e 1, sendo 1 não incluído:")
 e = float(input("e = ")) 
 
-print "Insira o ângulo (em graus) desejado do eixo-maior da elipse em relação ao eixo x1 cartesiano no sentido anti-horário:"
+print ("Insira o ângulo (em graus) desejado do eixo-maior da elipse em relação ao eixo x1 cartesiano no sentido anti-horário:")
 phie = float(input("phie = "))*(np.pi/180)
 
-print "Escolha o valor de n entre 1 e 10 da lei de Sérsic para simular o perfil de brilho do bojo de uma galáxia espiral (dica: a lei de de Vaucouleurs, n = 4, é uma ótima opção):"
+print ("Escolha o valor de n entre 1 e 10 da lei de Sérsic para simular o perfil de brilho do bojo de uma galáxia espiral (dica: a lei de de Vaucouleurs, n = 4, é uma ótima opção):")
 n = float(input("n = ")) 
 
-print "Escolha os valores para o bojo de Re (raio efetivo que contém metade da luminosidade total da galáxia) e Ie (brilho superficial isofota efetivo correspondente a Re) e para o disco escolha Io (brilho superficial central extrapolado) e Ro (distância entre o centro e o ponto do disco onde o brilho decai por um fator 1/e):"
+print ("Escolha os valores para o bojo de Re (raio efetivo que contém metade da luminosidade total da galáxia) e Ie (brilho superficial isofota efetivo correspondente a Re) e para o disco escolha Io (brilho superficial central extrapolado) e Ro (distância entre o centro e o ponto do disco onde o brilho decai por um fator 1/e):")
 S = float(input("Ie = "))
 Rs = float(input("Re = "))
 B = float(input("Io = "))
@@ -47,7 +47,7 @@ plt.xlabel('$x_1$')
 #plt.xlim(-1,1)
 plt.colorbar()
 
-print "Insira o valor para R0:"
+print ("Insira o valor para R0:")
 r0 = float(input("R0 = ")) 
 level = [S*10**(-(0.868*n-0.142)*((r0/Rs)**(1/n)-1)) + B*np.exp(-r0/Rb)]
 print('I(R0)', level)
--- SIS_arcs_elliptical.py
+++ SIS_arcs_elliptical.py
@@ -1,7 +1,10 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 np.seterr(divide='ignore', invalid='ignore')
 
+mpl.rcParams['mathtext.fontset'] = 'cm'
+#mpl.rcParams['font.family'] = 'STIXGeneral'
 
 # Image formation - SIS with circular source
 
@@ -9,21 +12,21 @@ phi = np.arange(0.0, 2.0*np.pi, 0.0001)
 xc1=np.cos(phi) #curva critica para eixo x1
 xc2=np.sin(phi) #curva critica para eixo x2
 
-#def xmenos(r0, s, e, phie):
-#   return np.where(1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(phi)-e*s*np.cos(phi-2*phie)-np.sqrt((r0**2)*(1-e*np.cos(2*(phi-phie)))-(s**2)*(1-e**2)*np.sin(phi)**2)))
+def xmenos(r0, s, theta, e, phie):
+    return np.where(1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(theta-phi)-e*s*np.cos(theta+phi-2*phie)-np.sqrt((r0**2)*(1-e*np.cos(2*(phi-phie)))-(s**2)*(1-e**2)*np.sin(theta-phi)**2))<0,np.nan,\
+      1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(theta-phi)-e*s*np.cos(theta+phi-2*phie)-np.sqrt((r0**2)*(1-e*np.cos(2*(phi-phie)))-(s**2)*(1-e**2)*np.sin(theta-phi)**2)))
 
-#def xmais(r0, s, e, phie):
-#    return np.where(1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(phi)-e*s*np.cos(phi-2*phie)+np.sqrt((r0**2)*(1-e*np.cos(2*(phi-phie)))-(s**2)*(1-e**2)*np.sin(phi)**2)))
+def xmais(r0, s, theta, e, phie):
+    return np.where(1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(theta-phi)-e*s*np.cos(theta+phi-2*phie)+np.sqrt((r0**2)*(1-e*np.cos(2*(phi-phie)))-(s**2)*(1-e**2)*np.sin(theta-phi)**2))<0,np.nan,\
+      1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(theta-phi)-e*s*np.cos(theta+phi-2*phie)+np.sqrt((r0**2)*(1-e*np.cos(2*(phi-phie)))-(s**2)*(1-e**2)*np.sin(theta-phi)**2)))
 
-r0   = float(input("R0 ="))
-s    = float(input("S ="))
-theta   = float(input("theta ="))*(np.pi/180)
-e    = float(input("e ="))
-phie = float(input("phie ="))*(np.pi/180) 
 
-xmenos = 1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(theta-phi)-e*s*np.cos(theta+phi-2*phie)-np.sqrt((r0**2)*(1-e*np.cos(2*(phi-phie)))-(s**2)*(1-e**2)*np.sin(theta-phi)**2))
-
-xmais = 1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(theta-phi)-e*s*np.cos(theta+phi-2*phie)+np.sqrt((r0**2)*(1-e*np.cos(2*(phi-phie)))-(s**2)*(1-e**2)*np.sin(theta-phi)**2))
+r0 = float(input("R0 = "))
+s = float(input("S = "))
+theta = float(input("theta = "))*(np.pi/180)
+e = float(input("e = "))
+phie = float(input("phie = "))*(np.pi/180) 
+    
 
 #subplot(nrows,nlines,position)
 
@@ -41,7 +44,7 @@ titlefont = {
         'size'   : 16,
         }
 
-ax1.set_title("Plano da Fonte", # title
+ax1.set_title("Plano das Fontes", # title
              va='bottom',                   # some space below the title
              fontdict = titlefont           # set the font properties
              )        
@@ -49,8 +52,9 @@ ax1.grid(False, which='both')
 ax1.axis([-2,2, -2, 2])
 ax1.axhline(y=0, color='0.9',ls='--',lw=1)
 ax1.axvline(x=0, color='0.9',ls='--',lw=1)
-plt.xlabel('$y_1$',fontsize=20)
-plt.ylabel('$y_2$',fontsize=20)
+ax1.set_aspect('equal', adjustable='box')
+plt.xlabel(r'$y_{1}$',fontsize=20)
+plt.ylabel(r'$y_{2}$',fontsize=20)
 
 #====================================================================================================
 # Image Formation - Lens Plane
@@ -58,10 +62,10 @@ plt.ylabel('$y_2$',fontsize=20)
 
 
 ax2 = plt.subplot(122)
-plt.plot(xc1, xc2, 'k--', lw=1, label='$x_c$')
-plt.plot(xmenos*np.cos(phi), xmenos*np.sin(phi), 'b-', lw=1, label='$x^{-}$')
-plt.plot(xmais*np.cos(phi), xmais*np.sin(phi), 'r-', lw = 1, label='$x^{+}$') 
-#plt.plot((1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(phi)-e*s*np.cos(phi-2*phie)))*np.cos(phi), (1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(phi)-e*s*np.cos(phi-2*phie)))*np.sin(phi) ,  'k--', lw=0.5, label=r'$\bar{x}$')
+plt.plot(xc1, xc2, 'k--', lw=1, label=r'$x_c$')
+plt.plot(xmenos(r0, s, theta, e, phie)*np.cos(phi), xmenos(r0, s, theta, e, phie)*np.sin(phi), 'b-', lw=1.3, label=r'$x^{-}$')
+plt.plot(xmais(r0, s, theta, e, phie)*np.cos(phi), xmais(r0, s, theta, e, phie)*np.sin(phi), 'r-', lw = 1.3, label=r'$x^{+}$') 
+plt.plot((1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(theta-phi)-e*s*np.cos(theta+phi-2*phie)))*np.cos(phi), (1+(1/(1-e*np.cos(2*(phi-phie))))*(s*np.cos(theta-phi)-e*s*np.cos(theta+phi-2*phie)))*np.sin(phi) ,  'k--', lw=0.5, label=r'$\bar{x}$')
 titlefont = {
         'family' : 'serif',
         'color'  : 'black',
@@ -69,7 +73,7 @@ titlefont = {
         'size'   : 16,
         }
 
-ax2.set_title("Plano da Lente", # title
+ax2.set_title("Plano das Lentes", # title
              va='bottom',                   # some space below the title
              fontdict = titlefont           # set the font properties
              )        
@@ -77,8 +81,10 @@ ax2.grid(False, which='both')
 ax2.axis([-2,2, -2, 2])
 ax2.axhline(y=0, color='0.9',ls='--',lw=1)
 ax2.axvline(x=0, color='0.9',ls='--',lw=1)
-plt.xlabel('$x_1$',fontsize=20)
-plt.ylabel('$x_2$',fontsize=20)
+ax2.set_aspect('equal', adjustable='box')
+plt.xlabel(r'$x_{1}$',fontsize=20)
+plt.ylabel(r'$x_{2}$',fontsize=20)
+
 ax2.legend(loc="lower left")           # legend location
 
 plt.show() 
